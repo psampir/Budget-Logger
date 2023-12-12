b1bdca5576fe043db0a4223dfbf9ce7992949644
psampir <pawel.sampir@gmail.com>
2023-12-12 23:16:53 +0100

    improved transaction history page

--- BudgetLogger/Models/Transaction.cs
+++ BudgetLogger/Models/Transaction.cs
@@ -4,19 +4,30 @@ using System.Text.Json.Serialization;
 namespace BudgetLogger.Models;
 
 public class Transaction
-{
-    [JsonPropertyName("amt")]
-    public decimal Amount { get; set; }
-    [JsonPropertyName("cat")]
-    public string Category { get; set; }
-    [JsonPropertyName("desc")]
-    public string Description { get; set; }
-    public DateTime DateTime { get; set; }
-    [JsonPropertyName("type")]
-    public TransactionType Type { get; set; }
+ {
+     [JsonPropertyName("amt")]
+     public decimal Amount { get; set; }
+     
+     [JsonPropertyName("cat")]
+     public string Category { get; set; }
+     
+     [JsonPropertyName("desc")]
+     public string Description { get; set; }
+     
+     [JsonPropertyName("datetime")]
+     public DateTime DateTime { get; set; }
+     
+     [JsonIgnore]
+     public string Date => DateTime.Date.ToString("yyyy-MM-dd");
 
-    public override string ToString() => JsonSerializer.Serialize<Transaction>(this);
-}
+     [JsonIgnore]
+     public TimeSpan Time => DateTime.TimeOfDay;
+     
+     [JsonPropertyName("type")]
+     public TransactionType Type { get; set; }
+ 
+     public override string ToString() => JsonSerializer.Serialize<Transaction>(this);
+ }
 
 public enum TransactionType
 {
